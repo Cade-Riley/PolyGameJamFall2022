fc5fb15acb06f8488f4036bed054e4f9ffc5c1f9
Mol-Dugaal <flubber9812@aol.com>
2022-09-19 14:17:27 -0400

    Updated PC Syntax
    Refactored the code to separate input from the position update.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float vSpeed = 5;
     public float hSpeed = 5;
+    private float hInput;
+    private float vInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(hSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,0, vSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(hSpeed * hInput * Time.deltaTime, 0, vSpeed * vInput * Time.deltaTime);
     }
 }
