9307c2cd1886bf99912a8c3b4b1095f90291d80f
Noremac3 <Camo32003@gmail.com>
2022-09-20 01:36:55 -0400

    Added Rotation
    Made gob rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float vSpeed = 5;
     public float hSpeed = 5;
+    private float hInput;
+    private float vInput;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(hSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,0, vSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        
+        transform.position = transform.position + new Vector3(hSpeed * hInput * Time.deltaTime, 0, vSpeed * vInput * Time.deltaTime);
+
+        direction = new Vector3((Input.GetAxisRaw("Horizontal")), 0, (Input.GetAxisRaw("Vertical")));
+        if (Mathf.Abs(direction.x) > 0.01 || Mathf.Abs(direction.z) > 0.01)
+        {
+            //transform.rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.15F);
+        }
+        Debug.Log(direction);
+
+        /*
+if (hInput > 0.01)
+{
+    //transform.Rotate(Vector3.up, 90f);
+    transform.eulerAngles = Vector3.up * 90;
+} 
+else if (hInput < -0.01)
+{
+    //transform.Rotate(Vector3.up, 90f);
+    transform.eulerAngles = Vector3.down * 90;
+}
+else if (vInput > 0.01)
+{
+    //transform.Rotate(Vector3.up, 90f);
+    transform.eulerAngles = Vector3.up * 180;
+}
+else if (vInput < -0.01)
+{
+    //transform.Rotate(Vector3.up, 90f);
+    transform.eulerAngles = Vector3.down * -180;
+}
+*/
+
+
     }
 }
